30229822f8fa3d6672ebb5ee17a75b26ba9403d5
HananSalemAlqarni <hsh7al@gmail.com>
2022-09-09 14:16:53 +0300

    new

---
+++ src/main/java/com/codingdojo/springmavenboot/SpringMavenBootT2Application.java
@@ -0,0 +1,13 @@
+package com.codingdojo.springmavenboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMavenBootT2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMavenBootT2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingdojo/springmavenboot/SpringMavenBootT2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.springmavenboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMavenBootT2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
